34565dad3c2c6a93155d65fdf9c07d39b88501f2
Leeroy Brown <brownleeroy010@gmail.com>
2026-01-14 18:40:59 +0200

    initial commit

---
+++ src/main/java/com/brown/asrs_documents_api/AsrsDocumentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.brown.asrs_documents_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsrsDocumentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsrsDocumentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brown/asrs_documents_api/AsrsDocumentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brown.asrs_documents_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsrsDocumentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
